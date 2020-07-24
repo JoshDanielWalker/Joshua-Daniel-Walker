e1628818d6520f586c355d18f65e9f12b6a6772b
Joshua walker <joshua.walker@sykescottages.co.uk>
2020-07-24 11:52:04 +0100

    update

--- name.js
+++ name.js
@@ -3,8 +3,20 @@ const urlParams = new URLSearchParams(queryString);
 const space = " ";
 var name = "";
 
+var title = "DevOps Engineer";
+
 if (urlParams.get('name') != null) {
   var name = urlParams.get('name');
   var formatname = space.concat(name);
   document.getElementById("name").innerHTML = formatname;
+}
+
+if (urlParams.get('title') != null) {
+  var title = urlParams.get('title');
+  var formattitle = space.concat(title);
+  document.getElementById("header").innerHTML = formattitle;
+  document.getElementById("title").innerHTML = formattitle;
+} else {
+  document.getElementById("header").innerHTML = title;
+  document.getElementById("title").innerHTML = title;
 }
